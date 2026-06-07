7bfab41d4379eb1ba1e6e8803781936b8a8e29e9
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-07 11:00:21 -0400

    Enable CORS on upload endpoint

--- backend-java/resumeanalyzer/src/main/java/com/dinkar/resumeanalyzer/controller/UploadController.java
+++ backend-java/resumeanalyzer/src/main/java/com/dinkar/resumeanalyzer/controller/UploadController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/resume")
 public class UploadController {
